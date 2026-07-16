c824efaaf809c9955f68e00e65da887c77e818a8
Auroshree Lenka <aurolenka2001@gmail.com>
2026-07-17 05:00:41 +0530

    Minor change for api versioning

--- src/main/java/com/interview/urlShortener/controller/UrlShortenerController.java
+++ src/main/java/com/interview/urlShortener/controller/UrlShortenerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/shorten")
+@RequestMapping({"/api/v1/shorten", "/shorten"})
 public class UrlShortenerController {
 
     private final UrlShortenerService urlShortenerService;
